d74152572318960c592c74f2ff8659a472a69c04
tino wibowo <buzoo@Buzoo-mini-macs-Mac-mini.local>
2016-03-17 18:15:04 +0700

    menu header hover dropdown

--- js/common.js
+++ js/common.js
@@ -34,4 +34,17 @@ jQuery(function($){
         $(".overlayer-shop").hide();
     });
     /*/box event hover*/
+
+    /* dropdown hover menu */
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+    /* /dropdown hover menu */
 });
